922b91c065ae7b89e46692f9bc20526179ecff6c
codecatroo <37349959+codecatroo@users.noreply.github.com>
2018-03-13 17:17:04 -0400

    initial commit

---
+++ Rylee/rylee's Project 2.py
@@ -0,0 +1,72 @@
+print("Hello! Welcome to the Python quiz!")
+
+print("Directions-Read and answer the question")
+    
+
+print("In Python what can you change or record")
+answer = raw_input()
+if answer == "list":
+    print("good job :)")
+else:
+    print("good try the answer was list")
+
+
+print("In Python what defines the level of visibilty to the rest of the program of a line of code?")
+answer = raw_input()
+if answer == "scope":
+    print("Awsome!")
+else:
+    print("Good Try The Answer Was scope!")
+
+
+print("In Python what do you call a programming conditional statement that, if proved true,will preform some action?")
+answer = raw_input()
+if answer == "if statement":
+    print("Your Good at this!")
+else:
+    print("It was if statement!")
+
+
+
+print("What do you call a piece of code that checks if a condition is proven to be true or false?")
+answer = raw_input()
+if answer == "conditional statement":
+    print("Excelent!")
+else:
+    print("Its conditional statement!")
+
+    
+print("What is a block of code that isinvoked by a "calling" program,best used to provide an autonomous service or calcalaton?
+print("A:list,B:init,or C:function")
+answer = raw_input()
+if answer =="c":
+      print("Great job!")
+else:
+      print("Good try it was c")
+
+print("What is a contaier class to hold and manipulate multiple sprites
+      
+
+
+
+print("In Python what repeats things over and over?")
+answer = raw_input()
+if answer == "while loop":
+    print("Good Job you finished the quiz!")
+else:
+    print("You Finished the quiz!The Answer is while loop")
+
+def getName():
+    name = raw_input("What is your name?")
+    print("Great job on the quiz! " + name)
+    print("Come back soon :)")
+
+
+
+
+
+
+
+
+
+
